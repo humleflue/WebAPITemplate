e72e515e97f216152402440b250a7e3f51629fa4
Lasse Skaalum <15886122+humleflue@users.noreply.github.com>
2021-10-16 21:07:03 +0200

    Small changes

--- WebAPITemplate/Controllers/UserController.cs
+++ WebAPITemplate/Controllers/UserController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WASPQueryAPI.Controllers;
 
-namespace ABK_Kasse_API.Controllers
+namespace WebAPITemplate.Controllers
 {
     public class UserController : UserControllerBase
     {
